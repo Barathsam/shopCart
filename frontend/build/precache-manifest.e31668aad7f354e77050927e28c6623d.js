e3b7bb6b3597a5ba18b13095db7b449ede9ac527
Akhila Vasa <akhila.vasa@gmail.com>
2022-01-27 17:39:03 +0530

    first commit

---
+++ frontend/build/precache-manifest.e31668aad7f354e77050927e28c6623d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45e5b81fdc11a99d61eb93c03ab76f07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3036122544d462d0ab53",
+    "url": "/static/css/main.b8d438e4.chunk.css"
+  },
+  {
+    "revision": "595da99cad7fb1e96c4a",
+    "url": "/static/js/2.2734b4af.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2734b4af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3036122544d462d0ab53",
+    "url": "/static/js/main.6093c69e.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
